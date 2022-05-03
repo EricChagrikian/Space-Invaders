7ad13113de9e4c358d12be14ba66af057d5bb54d
ozcanemre <ozcan.emre1608@gmail.com>
2022-05-03 12:23:33 +0200

    movemet

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let shooter = document.querySelector('.shooter')
+let grid = document.querySelector('.grid')
+
+
+
+window.addEventListener('load', () =>{
+shooter.style.position = 'absolute';
+shooter.style.left = 50+"%";
+shooter.style.bottom = 15+"px";
+});
+
+document.addEventListener('keydown', (e)=>{
+if(shooter.style.left===0+"%"){
+    console.log("limite");
+}
+else{
+    if(e.key === "ArrowLeft"){
+        shooter.style.left=parseInt(shooter.style.left) -2 +"%"
+        
+    }
+}
+if(shooter.style.right===0+"%"){
+    console.log("limite");
+}
+else{
+    if(e.key==='ArrowRight'){
+        shooter.style.left=parseInt(shooter.style.left) +2 +"%"
+    }
+    
+}
+}
+)
