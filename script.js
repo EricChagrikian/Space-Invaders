b9ac5a980531ecef26dbc29b2cf4d60444ee54f7
Eric Chagrikian <echagrikian@hotmail.com>
2022-05-03 12:23:28 +0200

    game over et you win

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let aliensRemoved = []
+const resultsDisplay = document.querySelector('.results')
+let results = 0
+
+
+
+draw()
+
+if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+    resultsDisplay.innerHTML = 'GAME OVER'
+    clearInterval(invadersId)
+}
+
+for (let i = 0; i < alienInvaders.length; i++) {
+    if(alienInvaders[i] > (squares.length)) {
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+    }
+}
+
+if (aliensRemoved.length === alienInvaders.length) {
+    resultsDisplay.innerHTML = "YOU WIN"
+    clearInterval(invadersId)
+}
+
+
 function shoot(e) {
     let laserId
     let currentLaserIndex = currentShooterIndex
@@ -5,6 +31,19 @@ function shoot(e) {
         squares[currentLaserIndex].classList.remove('laser')
         currentLaserIndex -= width
         squares[currentLaserIndex].classList.add('laser')
+
+        if (squares[currentLaserIndex].classList.contains('invader')) {
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+            clearInterval(laserId)
+            const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+            results++
+            resultsDisplay.innerHTML = results
+            aliensRemoved.push(alienRemoved)
+        }
     }
     switch(e.key) {
         case 'Space':
